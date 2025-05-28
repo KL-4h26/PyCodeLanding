2f63b194aea29a37f9aa8aafa6c32245b9736915
KL-4h26 <pcode922@gmail.com>
2025-05-28 10:27:36 +0000

    version 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 
-function changeTitle(title){
-    get_title = document.getElementsByTagName("title")
-    get_title.textContent = `PyCode - ${title}`
+const statuses = ["About", "Stack&Projects"]
+
+let contentMode = {
+    "About": {"title": "Приветствую, я - PyCode", "content": "Тут я рассказываю о себе и тд и тп"},
+    "Stack&Projects": {"title": "Немного о моем стеке и проектах", "content": "Использую то то и то то, проэкты такие то и такие то и можете мой гитхаб посетить"}
+}
+
+function nextPage(status){
+    // Изменение заголовка
+    let get_title = document.getElementsByTagName("title")
+    let tilte = document.getElementById("tlt")
+    let content = document.getElementById("content")
+
+    // Изменение содержимого
+    if (statuses.includes(status)){
+        get_title[0].textContent = `PyCode - ${status}`
+
+        tilte.textContent = contentMode[status]["title"]
+        content.textContent = contentMode[status]["content"]
+    }
 }
 
 
